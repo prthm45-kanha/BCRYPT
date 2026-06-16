953756f79451a68e2eb6aa4799dbd1aec21fc2c4
Pratham Agrawal <agrawalpratham2706@gmail.com>
2026-06-16 14:09:25 +0530

    donw with comaparing the password

--- app.js
+++ app.js
@@ -6,12 +6,20 @@ app.use(cookieParser());
 
 const bcrypt=require('bcrypt');
 
+// hashing the password 
 app.get('/',(req,res)=>{
     bcrypt.genSalt(10, function(err, salt) {
     bcrypt.hash("tusii@22", salt, function(err, hash) {
         console.log(salt);
        console.log(hash);
     });
+});
+})
+
+// comparing the password
+app.get('/',(req,res)=>{
+    bcrypt.compare("tusii@22", "$2b$10$O5go6ESwgSZ1DniNlz76zeIfA/YiEFddkF57Y.Zm7SWPJZSFmEiwy", function(err, result) {
+   console.log(result)
 });
 })
 
