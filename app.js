ffb82bab6cb8323e05666b9ac633dbfc3fd686a5
Pratham Agrawal <agrawalpratham2706@gmail.com>
2026-06-16 13:29:12 +0530

    done with setting up the cookie and reading the cookie

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
+const cookieParser=require('cookie-parser');
 const express=require('express');
 const app=express();
 
+app.use(cookieParser());
+
 app.get('/',(req,res)=>{
-    res.send('Hello World');
+    res.cookie('name','Pratham Agrawal Bansal');
+    res.send('done');
+})
+app.get('/read',(req,res)=>{
+    console.log(req.cookies);
+    res.send('read page');
 })
 
 app.listen(3000,()=>{
