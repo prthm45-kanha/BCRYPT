fa9988e02784074e978830e96f4b12b30631bf7a
Pratham Agrawal <agrawalpratham2706@gmail.com>
2026-06-16 14:02:10 +0530

    done hashing of password using bcrypt

--- app.js
+++ app.js
@@ -4,14 +4,27 @@ const app=express();
 
 app.use(cookieParser());
 
+const bcrypt=require('bcrypt');
+
 app.get('/',(req,res)=>{
-    res.cookie('name','Pratham Agrawal Bansal');
-    res.send('done');
+    bcrypt.genSalt(10, function(err, salt) {
+    bcrypt.hash("tusii@22", salt, function(err, hash) {
+        console.log(salt);
+       console.log(hash);
+    });
+});
 })
-app.get('/read',(req,res)=>{
-    console.log(req.cookies);
-    res.send('read page');
-})
+
+// set up the cookie
+// app.get('/',(req,res)=>{
+//     res.cookie('name','Pratham Agrawal Bansal');
+//     res.send('done');
+// })
+// read the cookie
+// app.get('/read',(req,res)=>{
+//     console.log(req.cookies);
+//     res.send('read page');
+// })
 
 app.listen(3000,()=>{
     console.log('server running on port 3000');
